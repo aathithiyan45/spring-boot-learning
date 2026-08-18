ebf62f15117b8af0813cad9f862df64993a26995
aathithiyan45 <aathimi07012006@gmail.com>
2026-08-18 09:29:59 +0530

    Day 2 - Spring Boot basics

---
+++ src/com/aathithiyan/App.java
@@ -0,0 +1,11 @@
+package com.aathithiyan;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("My first Maven project");
+    }
+}
